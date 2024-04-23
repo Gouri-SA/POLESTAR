68d8dd0d95c4d48c82e3fbde01fd5da6c46dcbef
Gouri-SA <109664288+Gouri-SA@users.noreply.github.com>
2024-04-23 19:26:42 +0530

    Delete uiPage.ts

--- uiPage.ts
+++
@@ -1,9 +0,0 @@
-import { Page } from '@playwright/test';
-
-
-export const Url='https://www.polestar.com/se/developer/get-started/';
-
-export const subscribeButton='//span[text()="Subscribe"]';
-
-
-  
